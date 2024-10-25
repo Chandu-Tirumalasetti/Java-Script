1cb4e1f9cbaa5e58769fed0bd3025bbda6417222
Chandu-Tirumalasetti <chanduchandu81656@gmail.com>
2024-10-25 17:59:41 +0530

    java script

---
+++ Day2 Task/Task.js
@@ -0,0 +1,69 @@
+alert('Hii This is External');
+
+// VARIABLES DECLARATION AND INITIALIZATION
+const a=10;
+console.log(a);
+
+var b="Chandu";
+console.log(b);
+
+let c;
+console.log(c);
+// DATATYPES
+ var a1=20;
+ console.log(a1);
+ 
+ var b1="hello world";
+ console.log(b1);
+ 
+ var c1;
+ console.log(c1);
+
+ var d1=null;
+ console.log(d1);
+
+ var e1=true;
+ console.log(e1);
+
+ console.log(typeof(a1));
+ console.log(typeof(b1));
+ console.log(typeof(c1));
+ console.log(typeof(d1));
+ console.log(typeof(e1));
+
+ var a2=true;
+ var b2=false;
+ console.log(a2+b2);
+ console.log(a2-b2);
+ console.log(a2+a2+a2);
+ console.log(b2-a2);
+ console.log(a2-a2);
+
+//  Non-Primitive Data Types
+var a3=["hello","world","20", "30"];
+console.log(a3);
+console.log(a3[1]);
+
+var a3=["hello","world","20", "30"];
+a3[1]="chandu"
+console.log(a3);
+
+
+
+var b3={
+        nanna:"Surya",
+        amma:"Bharathi",
+        nenu:"Chandu",
+        thammudu:"prasad"
+       }
+console.log(b3);
+console.log(b3.nanna,b3.amma);
+
+var b3={
+        nanna:"Surya",
+        amma:"Bharathi",
+        nenu:"Chandu",
+        thammudu:"prasad"
+       }
+b3.thammudu="Jithendra"       
+console.log(b3);
